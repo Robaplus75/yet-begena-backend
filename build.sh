3c2baef19112f56f3fdab8baa6fba20e57d5155e
robel <robelwon75@gmail.com>
2025-06-10 01:08:08 +0300

    last deploy3

--- build.sh
+++ build.sh
@@ -4,8 +4,10 @@ set -o errexit
 
 pip install -r requirements.txt
 
+python manage.py makemigrations
+python manage.py migrate
+
 python manage.py collectstatic --no-input
 
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'yourpassword')" | python manage.py shell
 
-python manage.py migrate
