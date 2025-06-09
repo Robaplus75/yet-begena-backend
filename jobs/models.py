ec67a35b636e00fe90490eb746a22149641f7b2c
robel <robelwon75@gmail.com>
2025-06-10 00:24:36 +0300

    updates

--- jobs/models.py
+++ jobs/models.py
@@ -25,8 +25,7 @@ class Job(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
     course = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True, blank=True, related_name='related_jobs')
-
-    # posted_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posted_jobs')
+    posted_by = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
         return self.title
